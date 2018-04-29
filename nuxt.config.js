cb35a6934d37258c8639d73bd2d526ecf7c8de2f
owacon <owacon.tokyo@gmail.com>
2018-04-29 23:29:47 +0900

    buefy入れた。

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,53 +1,71 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'owacon-blog',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
+  },
+  plugins: [
+    '~plugins/buefy'
+  ],
+  /*
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070'
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
- buildDir: 'functions/nuxt',
- build: {
-   publicPath: '/assets/',
-   extractCSS: true,
-   babel: {
-     presets: [
-       'es2015',
-       'stage-0'
-     ],
-     plugins: [
-       ['transform-runtime', {
-         polyfill: true,
-         regenerator: true
-       }],
-     ],
-   },
-   /*
-   ** Run ESLint on save
+   ** Build configuration
    */
-   extend (config, { isDev, isClient }) {
-     if (isDev && isClient) {
-       config.module.rules.push({
-         enforce: 'pre',
-         test: /\.(js|vue)$/,
-         loader: 'eslint-loader',
-         exclude: /(node_modules)/
-       })
-     }
-   }
- }
+  buildDir: 'functions/nuxt',
+  build: {
+    publicPath: '/assets/',
+    extractCSS: true,
+    babel: {
+      presets: [
+        'es2015',
+        'stage-0'
+      ],
+      plugins: [
+        ['transform-runtime', {
+          polyfill: true,
+          regenerator: true
+        }],
+      ],
+    },
+    /*
+     ** Run ESLint on save
+     */
+    extend(config, {
+      isDev,
+      isClient
+    }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
-<template>
+<template lnag="pug">
   <section class="container">
     <div>
       <app-logo/>
@@ -9,56 +9,65 @@
         Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
+    <b-collapse :open="false">
+      <button class="button is-primary" slot="trigger">Click me!</button>
+      <div class="notification">
+        <div class="content">
+          <h3>
+            Subtitle
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/> Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. <br/> Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat
+            vel, interdum mattis neque.
+          </p>
+        </div>
+      </div>
+    </b-collapse>
   </section>
 </template>
 
-<script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
+<style lang="scss" scopde>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
   }
-}
-</script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  
+  .title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+  
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+  
+  .links {
+    padding-top: 15px;
+  }
 </style>
+
+<script>
+  import AppLogo from "~/components/AppLogo.vue";
+  
+  export default {
+    components: {
+      AppLogo
+    }
+  };
+</script>
