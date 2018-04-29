130fe1be891c71b85a003499b0a44383606f027f
owacon <owacon.tokyo@gmail.com>
2018-04-29 19:13:49 +0900

    nuxt、firebase等の設定を完了。

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,19 +20,34 @@ module.exports = {
   /*
   ** Build configuration
   */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+ buildDir: 'functions/nuxt',
+ build: {
+   publicPath: '/assets/',
+   extractCSS: true,
+   babel: {
+     presets: [
+       'es2015',
+       'stage-0'
+     ],
+     plugins: [
+       ['transform-runtime', {
+         polyfill: true,
+         regenerator: true
+       }],
+     ],
+   },
+   /*
+   ** Run ESLint on save
+   */
+   extend (config, { isDev, isClient }) {
+     if (isDev && isClient) {
+       config.module.rules.push({
+         enforce: 'pre',
+         test: /\.(js|vue)$/,
+         loader: 'eslint-loader',
+         exclude: /(node_modules)/
+       })
+     }
+   }
+ }
 }
